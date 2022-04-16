e47d2f2068c929146bd9d4191867808182bb9e1f
Philip Maina <maina.zak@nobilisltd.co.ke>
2022-04-17 00:50:15 +0300

    chore: Remove unnecessary comments.

--- finite_automaton/non_deterministic_finite_automaton/strategies/base_strategy.rb
+++ finite_automaton/non_deterministic_finite_automaton/strategies/base_strategy.rb
@@ -1,7 +1,3 @@
-# NonDeterministicFiniteAutomaton is a class of its own as seen in 
-# finite_automaton/non_determistic_finite_automaton.rb . But it's also
-# a good namespace for its dependent entities. We can't say module NonDeterministicFiniteAutomaton 
-# because that constant is already defined as a class, but we can still use it as a namespace.
 module FiniteAutomaton
   class NonDeterministicFiniteAutomaton 
     module Strategies
--- pushdown_automaton/non_deterministic_pushdown_automaton/strategies/base_strategy.rb
+++ pushdown_automaton/non_deterministic_pushdown_automaton/strategies/base_strategy.rb
@@ -1,7 +1,3 @@
-# NonDeterministicPushdownAutomaton is a class of its own as seen in 
-# pushdown_automaton/non_determistic_pushdown_automaton.rb . But it's also
-# a good namespace for its dependent entities. We can't say module NonDeterministicPushdownAutomaton 
-# because that constant is already defined as a class, but we can still use it as a namespace.
 module PushdownAutomaton 
   class NonDeterministicPushdownAutomaton 
     module Strategies
